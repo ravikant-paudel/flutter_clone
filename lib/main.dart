1eed1026f0d9702615f39495806baa496946176e
Ravikant <ravikant.paudel@gmail.com>
2020-04-16 21:32:51 +0545

    Working in Protfolio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,57 +7,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100),
+              Text(
+                'Mobile Developer',
+                style: TextStyle(fontSize: 32),
+              ),
+              SizedBox(height: 30),
+              CircleAvatar(radius: 80),
+              SizedBox(height: 30),
+              Text(
+                'Ravikant Paudel',
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 30),
+              Column(
+                children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.mail_outline, size: 22),
+                        SizedBox(width: 10),
+                        Text('Here', style: TextStyle(fontSize: 22))
+                      ]),
+                  SizedBox(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.location_on, size: 22),
+                        SizedBox(width: 10),
+                        Text('Here', style: TextStyle(fontSize: 22))
+                      ]),
+                  SizedBox(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.branding_watermark, size: 22),
+                        SizedBox(width: 10),
+                        Text('Here', style: TextStyle(fontSize: 22))
+                      ]),
+                  SizedBox(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.account_circle, size: 22),
+                        SizedBox(width: 10),
+                        Text('Here', style: TextStyle(fontSize: 22))
+                      ]),
+                  SizedBox(height: 10),
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
